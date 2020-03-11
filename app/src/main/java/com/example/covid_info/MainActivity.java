238b388012df7963e2284434dcebddb45071e71d
HQalphaca <meddugi7170@gmail.com>
2020-03-11 17:51:35 +0900

    2020.03.11 수정내용 1. 홈의 내비게이션 메뉴바를 통한 이동이 아닌 홈에서 버튼을 누르면 이동할수있게 메인화면 변경(디스코드의 2차 디자인안 참고) 2. 각 창에서 HOME버튼을 누르면 메인화면으로 이동하도록 기능 수정 3. 메인화면에서 뒤로가기 버튼을 눌러도 바로 종료되지 않도록 하는 기능 추가

--- app/src/main/java/com/example/covid_info/MainActivity.java
+++ app/src/main/java/com/example/covid_info/MainActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,38 +13,26 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+
     private Button btn_map,btn_situation,btn_info,btn_med,btn_foreign,btn_notice;   //버튼 변수 설정
-    private DrawerLayout drawerLayout;  //DrawerLayout 변수설정
-    private View drawerView;    //drawerView 변수 설정
+    private final long FINISH_INTERVAL_TIME = 2000;//2초안에 뒤로가기를 한번 더 누르면 종료되도록 2000설정
+    private long backPressedTime =0; //Back키를 눌렀을때 시간 0초 ~2초
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main); //메인액티비티에대한 설정
 
 
-        drawerLayout = (DrawerLayout)findViewById(R.id.drawer_layout);  //DrawerLayout 에 대한 id 설정
-        drawerView = (View)findViewById(R.id.drawer);   //DrawerView 에 대한 id 설정
 
-        Button btn_open = (Button)findViewById(R.id.btn_open);
-        btn_open.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.openDrawer(drawerView);
-            }
-        });//btn_open id가 있는 버튼을 입력했을 때 일어나는 일 설정
-        Button btn_close = (Button)findViewById(R.id.btn_close);
-        btn_close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                drawerLayout.closeDrawers();
-            }
-        });//btn_close id가 있는 버튼을 입력했을떄 일어나는 일 설정
+
+
 
         btn_map=findViewById(R.id.btn_map);
         btn_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,MapActivity.class);
+                startActivity(intent);
             }
         });//btn_map id가 있는 버튼 입력시 MapActivity로 이동
         btn_situation=findViewById(R.id.btn_situation);
@@ -51,6 +40,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,SituationActivity.class);
+                startActivity(intent);
             }
         });//btn_situation id가 있는 버튼 입력시 SituationActivity로 이동
         btn_info=findViewById(R.id.btn_info);
@@ -58,6 +48,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,InfoActivity.class);
+                startActivity(intent);
             }
         });//btn_info id가 있는 버튼 입력시 InfoActivity로 이동
         btn_med=findViewById(R.id.btn_med);
@@ -65,6 +56,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,MedActivity.class);
+                startActivity(intent);
             }
         });//btn_med id가 있는 버튼 입력시 MedActivity로 이동
         btn_foreign=findViewById(R.id.btn_foreign);
@@ -72,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,ForeignActivity.class);
+                startActivity(intent);
             }
         });//btn_foreign id가 있는 버튼 입력시 ForeignActivity로 이동
         btn_notice=findViewById(R.id.btn_notice);
@@ -79,44 +72,23 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this,NoticeActivity.class);
+                startActivity(intent);
             }
         });//btn_notice id가 있는 버튼 입력시 NoticeActivity로 이동
 
-        drawerLayout.setDrawerListener(listener);//drawerLayout 리스너 등록
-        drawerView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View v, MotionEvent event) {
-                return true;
-            }
-        });//drawerView에대한 리스너 등록
-
-
-
-
-
-
 
     }
+    @Override
+    public void onBackPressed(){
+        long tempTime = System.currentTimeMillis();
+        long intervalTime = tempTime -backPressedTime;
 
-    DrawerLayout.DrawerListener listener = new DrawerLayout.DrawerListener() {
-        @Override
-        public void onDrawerSlide(@NonNull View drawerView, float slideOffset) {
-
+        if(0<=intervalTime&&FINISH_INTERVAL_TIME>=intervalTime){
+            super.onBackPressed();
         }
-
-        @Override
-        public void onDrawerOpened(@NonNull View drawerView) {
-
+        else{
+            Toast.makeText(getApplicationContext(),"뒤로버튼을 한번더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
         }
+    }
 
-        @Override
-        public void onDrawerClosed(@NonNull View drawerView) {
-
-        }
-
-        @Override
-        public void onDrawerStateChanged(int newState) {
-
-        }
-    };//DrawerLayout의 상태값에 따른 Listener설정
 }
--- app/src/main/java/com/example/covid_info/SituationActivity.java
+++ app/src/main/java/com/example/covid_info/SituationActivity.java
@@ -1,14 +1,28 @@
 package com.example.covid_info;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SituationActivity extends AppCompatActivity {
 
+    private Button btnHome;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_situation);
+
+        btnHome=findViewById(R.id.btnHome);
+        btnHome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SituationActivity.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
