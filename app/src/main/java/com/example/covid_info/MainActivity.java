ab7b457f652807e8a0678f6a91b69a56b8ed2f96
HQalphaca <meddugi7170@gmail.com>
2020-03-13 17:44:51 +0900

    2020.03.13.작업내용 1.타이틀바 제거 2.뒤로가기 버튼을 두번 눌러도 종료되지 않던 문제 해결

--- app/src/main/java/com/example/covid_info/MainActivity.java
+++ app/src/main/java/com/example/covid_info/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends AppCompatActivity {
 
     private Button btn_map,btn_situation,btn_info,btn_med,btn_foreign,btn_notice;   //버튼 변수 설정
     private final long FINISH_INTERVAL_TIME = 2000;//2초안에 뒤로가기를 한번 더 누르면 종료되도록 2000설정
-    private long backPressedTime =0; //Back키를 눌렀을때 시간 0초 ~2초
+    private long Back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
         btn_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"moved successful",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this,MapActivity.class);
                 startActivity(intent);
             }
@@ -41,7 +40,6 @@ public class MainActivity extends AppCompatActivity {
         btn_situation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"moved successful",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this,SituationActivity.class);
                 startActivity(intent);
             }
@@ -50,7 +48,6 @@ public class MainActivity extends AppCompatActivity {
         btn_info.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"moved successful",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this,InfoActivity.class);
                 startActivity(intent);
             }
@@ -59,7 +56,6 @@ public class MainActivity extends AppCompatActivity {
         btn_med.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"moved successful",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this,MedActivity.class);
                 startActivity(intent);
             }
@@ -68,7 +64,6 @@ public class MainActivity extends AppCompatActivity {
         btn_foreign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"moved successful",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this,ForeignActivity.class);
                 startActivity(intent);
             }
@@ -77,7 +72,6 @@ public class MainActivity extends AppCompatActivity {
         btn_notice.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getApplicationContext(),"moved successful",Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this,NoticeActivity.class);
                 startActivity(intent);
             }
@@ -87,15 +81,11 @@ public class MainActivity extends AppCompatActivity {
     }
     @Override
     public void onBackPressed(){
-        long tempTime = System.currentTimeMillis();
-        long intervalTime = tempTime -backPressedTime;
-
-        if(0<=intervalTime&&FINISH_INTERVAL_TIME>=intervalTime){
-            super.onBackPressed();
+        if(System.currentTimeMillis()-Back<2000){
+            finish();
         }
-        else{
-            Toast.makeText(getApplicationContext(),"뒤로버튼을 한번더 누르시면 종료됩니다.", Toast.LENGTH_SHORT).show();
-        }
+        Toast.makeText(this,"뒤로가기 버튼을 한번더 누르면 종료됩니다.",Toast.LENGTH_SHORT).show();
+        Back=System.currentTimeMillis();
     }
 
 }
