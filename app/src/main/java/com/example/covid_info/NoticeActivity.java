c74f34a2107014a27adb0a3658c5f99b43bb502d
HQalphaca <meddugi7170@gmail.com>
2020-03-21 13:33:00 +0900

    누락본 수정

--- app/src/main/java/com/example/covid_info/NoticeActivity.java
+++ app/src/main/java/com/example/covid_info/NoticeActivity.java
@@ -4,12 +4,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class NoticeActivity extends AppCompatActivity {
 
-    private Button btnHome;
+    private ImageButton btnHome;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
