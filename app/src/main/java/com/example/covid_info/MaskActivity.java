96638d576e5247e4642ec99d7a7272b97f1fbe64
HQalphaca <meddugi7170@gmail.com>
2020-06-15 23:30:33 +0900

    no message

--- app/src/main/java/com/example/covid_info/MaskActivity.java
+++ app/src/main/java/com/example/covid_info/MaskActivity.java
@@ -3,14 +3,26 @@ package com.example.covid_info;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MaskActivity extends AppCompatActivity {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MaskActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private ImageButton btnHome;
+    private FragmentManager fragmentManager;
+    private MapFragment mapFragment;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -25,5 +37,21 @@ public class MaskActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        fragmentManager=getFragmentManager();
+        mapFragment=(MapFragment)fragmentManager.findFragmentById(R.id.googlemap);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        LatLng location = new LatLng(37.303067, 126.866490);//상록수역
+        MarkerOptions markerOptions = new MarkerOptions();
+        markerOptions.title("상록수역");
+        markerOptions.snippet("전철역");
+        markerOptions.position(location);
+        googleMap.addMarker(markerOptions);
+
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(location,16));
     }
 }
