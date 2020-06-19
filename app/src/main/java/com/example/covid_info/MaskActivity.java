5e3cbeee7daf02689d59eeaa444d1e2eab6a9071
blackarea <supersute142@gmail.com>
2020-06-19 13:02:05 +0900

    마스크 보이고 카메라 이동에 따라서 마스크도 옮겨짐

--- app/src/main/java/com/example/covid_info/MaskActivity.java
+++ app/src/main/java/com/example/covid_info/MaskActivity.java
@@ -1,6 +1,7 @@
 package com.example.covid_info;
 
 import android.content.Intent;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -10,14 +11,36 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 
+import com.naver.maps.geometry.LatLng;
+import com.naver.maps.map.LocationTrackingMode;
 import com.naver.maps.map.MapFragment;
 import com.naver.maps.map.NaverMap;
 import com.naver.maps.map.OnMapReadyCallback;
+import com.naver.maps.map.UiSettings;
+import com.naver.maps.map.overlay.InfoWindow;
+import com.naver.maps.map.overlay.Marker;
+import com.naver.maps.map.overlay.Overlay;
+import com.naver.maps.map.overlay.OverlayImage;
+import com.naver.maps.map.util.FusedLocationSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
-public class MaskActivity extends AppCompatActivity implements OnMapReadyCallback {
+public class MaskActivity extends AppCompatActivity implements NaverMap.OnMapClickListener, Overlay.OnClickListener, OnMapReadyCallback, NaverMap.OnCameraChangeListener, NaverMap.OnCameraIdleListener {
 
     private ImageButton btnHome;
+    private static final int ACCESS_LOCATION_PERMISSION_REQUEST_CODE = 100;
+    private FusedLocationSource locationSource;
+    private NaverMap naverMap;
+    private List<Marker> markerList = new ArrayList<Marker>();
+    private boolean isCameraAnimated = false;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -39,12 +62,128 @@ public class MaskActivity extends AppCompatActivity implements OnMapReadyCallbac
             mapFragment = MapFragment.newInstance();
             fm.beginTransaction().add(R.id.map, mapFragment).commit();
         }
+        mapFragment.getMapAsync(this);
 
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (locationSource.onRequestPermissionsResult(
+                requestCode, permissions, grantResults)) {
+            if (!locationSource.isActivated()) { // 권한 거부됨
+                naverMap.setLocationTrackingMode(LocationTrackingMode.None);
+            }
+            return;
+        }
+        super.onRequestPermissionsResult(
+                requestCode, permissions, grantResults);
+    }
 
     @Override
     public void onMapReady(@NonNull NaverMap naverMap) {
+        this.naverMap = naverMap;
 
+        locationSource = new FusedLocationSource(this, ACCESS_LOCATION_PERMISSION_REQUEST_CODE);
+        naverMap.setLocationSource(locationSource);
+        UiSettings uiSettings = naverMap.getUiSettings();
+        uiSettings.setLocationButtonEnabled(true);
+
+        naverMap.addOnCameraChangeListener(this);
+        naverMap.addOnCameraIdleListener(this);
+
+        LatLng mapCenter = naverMap.getCameraPosition().target;
+        fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
+    }
+
+    /**
+     * 카메라 이동중에는 말고 이동이 끝났을때를 상태저장한다.
+     * @param animated
+     */
+    @Override
+    public void onCameraChange(int i, boolean animated) {
+        isCameraAnimated = animated;
+    }
+
+    /**
+     *  카메라 이동시 fetchStoreSale를 다시 불러와 그 위치에 맞는 마스크 정보를 다시 불러온다.
+     */
+    @Override
+    public void onCameraIdle() {
+        if (isCameraAnimated) {
+            LatLng mapCenter = naverMap.getCameraPosition().target;
+            fetchStoreSale(mapCenter.latitude, mapCenter.longitude, 5000);
+        }
+    }
+
+
+    /**
+     * 지도에 마스크를 표시해준다.
+     *
+     * @param lat 위도
+     * @param lng 경도
+     * @param m   반경 (현재 위치에서 몇 m까지 마스크를 표시할 건지)
+     */
+    private void fetchStoreSale(double lat, double lng, int m) {
+        Retrofit retrofit = new Retrofit.Builder().baseUrl("https://8oi9s0nnth.apigw.ntruss.com").addConverterFactory(GsonConverterFactory.create()).build();
+        MaskApi maskApi = retrofit.create(MaskApi.class);
+        maskApi.getStoresByGeo(lat, lng, m).enqueue(new Callback<StoreSaleResult>() {
+            @Override
+            public void onResponse(Call<StoreSaleResult> call, Response<StoreSaleResult> response) {
+                if (response.code() == 200) {
+                    StoreSaleResult result = response.body();
+                    updateMapMarkers(result);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<StoreSaleResult> call, Throwable t) {
+
+            }
+        });
+    }
+
+
+
+    private void updateMapMarkers(StoreSaleResult result) {
+        resetMarkerList();
+        if (result.stores != null && result.stores.size() > 0) {
+            for (Store store : result.stores) {
+                Marker marker = new Marker();
+                marker.setTag(store);
+                marker.setPosition(new LatLng(store.lat, store.lng));
+                if ("plenty".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_green));
+                } else if ("some".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_yellow));
+                } else if ("fiew".equalsIgnoreCase(store.remain_stat)) {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_red));
+                } else {
+                    marker.setIcon(OverlayImage.fromResource(R.drawable.marker_gray));
+                }
+                marker.setAnchor(new PointF(0.5f, 1.0f));
+                marker.setMap(naverMap);
+                //marker.setOnClickListener(this);
+                markerList.add(marker);
+            }
+        }
+    }
+
+    private void resetMarkerList() {
+        if (markerList != null && markerList.size() > 0) {
+            for (Marker marker : markerList) {
+                marker.setMap(null);
+            }
+            markerList.clear();
+        }
+    }
+
+    @Override
+    public void onMapClick(@NonNull PointF pointF, @NonNull LatLng latLng) {
+
+    }
+
+    @Override
+    public boolean onClick(@NonNull Overlay overlay) {
+        return false;
     }
 }
